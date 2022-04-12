61b06f1918c8a146000eb8ffc28449c32156ad42
Devendra Arya <devendra3583@gmail.com>
2022-04-12 13:36:04 +0530

    20220412 2

--- tmp2/script/run.js
+++ tmp2/script/run.js
@@ -1,4 +1,4 @@
-     app.run(['$rootScope', 'appConfig', '$translate', function ($rootScope, appConfig, $translate) {
+     app.run(['$rootScope', 'appConfig', '$translate', '$http', function ($rootScope, appConfig, $translate, $http) {
                     $rootScope.WEBSITE_NAME = appConfig.YOUR_WEBSITE_NAME;
                     if(window.localStorage.getItem("languageSelected")){
                         $translate.use(window.localStorage.getItem("languageSelected"));
@@ -23,4 +23,20 @@
                         }
                     };
                     $rootScope.selectedLanguage = $rootScope.languages[0];
+                    $rootScope.views_on_website_including_pageviews = 0;
+                    $http.get('json/v.json', {}).then(function successCallback(response){
+                            //console.log("success",response);
+                            if(response.status == 200){
+                                $rootScope.views_on_website_including_pageviews = response.data.views_on_website_including_pageviews_since_20220412;
+                            };
+                        }, 
+                            function errorCallback(response){
+                                //console.log("error",response);
+                    });
+                    
+                    $rootScope.$on('$stateChangeStart', function(e, toState) {
+                        $rootScope.views_on_website_including_pageviews += 1;
+                    });
+
+
                 }]);
